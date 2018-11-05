2213df1acab15fd22afaad670453f9882296c561
saulo.cardoso <saulo.cardoso@softplan.com.br>
2018-11-05 17:47:20 -0200

    Task- configuração clientes acesso ao banco

--- gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/repository/Clientes.java
+++ gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/repository/Clientes.java
@@ -3,7 +3,9 @@ package com.gerenciadorClin.brewer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gerenciadorClin.brewer.model.Cliente;
+
 @Repository
-public interface Clientes extends JpaRepository<Clientes, Long>{
+public interface Clientes extends JpaRepository<Cliente, Long>{
 
 }
