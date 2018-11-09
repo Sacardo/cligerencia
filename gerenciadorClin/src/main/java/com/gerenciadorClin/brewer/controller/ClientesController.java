150da1347664ad36990398cc6e376109e6c07a3e
saulo.cardoso <saulo.cardoso@softplan.com.br>
2018-11-09 16:54:29 -0200

    Task- Sonatype-nexus-snapshots -depedencia - consultaclientes

--- gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/controller/ClientesController.java
+++ gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/controller/ClientesController.java
@@ -2,7 +2,6 @@ package com.gerenciadorClin.brewer.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gerenciadorClin.brewer.model.Cliente;
-import com.gerenciadorClin.brewer.repository.Clientes;
 
 @Controller
 public class ClientesController {
 	
 /*	@Autowired
 	private Clientes clientes;*/
+	
+	@RequestMapping("/clientes/pesquisar")	
+	public String pesquisar(Cliente cliente) {
+		return "cliente/pesquisaClientes2";
+	}
 	
 	@RequestMapping("/clientes/novo")	
 	public String novo(Cliente cliente) {
