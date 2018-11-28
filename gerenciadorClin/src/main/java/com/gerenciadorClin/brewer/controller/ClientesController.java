a7fe6adb1880bb7ee7aab202b3eb6570a32e0a83
saulo <saulo@DESKTOP-OLD4510>
2018-11-28 00:37:42 -0200

    Task - atualizacao controller class clientes

--- gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/controller/ClientesController.java
+++ gerenciadorClin/src/main/java/com/gerenciadorClin/brewer/controller/ClientesController.java
@@ -2,40 +2,51 @@ package com.gerenciadorClin.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gerenciadorClin.brewer.model.Cliente;
+import com.gerenciadorClin.brewer.repository.Clientes;
 
 @Controller
 public class ClientesController {
 	
-/*	@Autowired
-	private Clientes clientes;*/
+	@Autowired
+	private Clientes clientes;
 	
 	@RequestMapping("/clientes/pesquisar")	
-	public String pesquisar(Cliente cliente) {
-		return "cliente/pesquisarClientes";
+	public ModelAndView pesquisar(Cliente cliente) {
+		return ModelAndView("cliente/pesquisarClientes");
 	}
 	
 	@RequestMapping("/clientes/novo")	
-	public String novo(Cliente cliente) {
-		return "cliente/cadastroClientes";
+	public ModelAndView novo(Cliente cliente) {
+		ModelAndView vw = new ModelAndView("cliente/cadastroClientes");
+		vw.addObject("cliente",((JpaRepository<Cliente, Long>) cliente).findAll());
+		return vw;
 	}
 	
 	@RequestMapping(value = "/clientes/novo", method = RequestMethod.POST)
-	public String cadastro(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastro(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cliente);
 		}
 		attributes.addFlashAttribute("mensagem","Cliente salvo com sucesso!");
-		return "redirect:/clientes/novo";
+		return ModelAndView ("redirect:/clientes/novo");
 	}
 	
+	private ModelAndView ModelAndView(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@RequestMapping("/clientes/cadastro")
 	public String cadastro(){
 		return "cliente/cadastro-produto";
